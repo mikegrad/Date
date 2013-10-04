d658688d536aa936e308b4242a7697c46570871f
mikegrad <mikegrad@yahoo.com>
2013-10-03 20:36:35 -0400

    Initial Commit

---
+++ Date/DateAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DateAppDelegate.h
+//  Date
+//
+//  Created by Michael Gradilone on 10/3/13.
+//  Copyright (c) 2013 FinTechSolutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DateAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Date/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Date
+//
+//  Created by Michael Gradilone on 10/3/13.
+//  Copyright (c) 2013 FinTechSolutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DateAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([DateAppDelegate class]));
+    }
+}
